626201800611690c1c07eaae125ee783ff9bcd4a
Sophie Long <84780493+5eastar@users.noreply.github.com>
2026-05-03 15:51:37 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,471 @@
+// Auto-generated quiz data
+// Generated on: 18/02/2026, 17:04:07
+// Total Programs: 6
+// Total Stimuli: 54
+// Total Images: 134
+
+window.quizData = {
+  "programs": [
+    {
+      "name": "Home Items",
+      "stimulus": [
+        {
+          "name": "Chair",
+          "images": [
+            "images/homeItems/chair1.jpg",
+            "images/homeItems/chair2.jpeg",
+            "images/homeItems/chair3.jpg",
+            "images/homeItems/chair4.jpg"
+          ]
+        },
+        {
+          "name": "Curtains",
+          "images": [
+            "images/homeItems/curtains1.jpg",
+            "images/homeItems/curtains2.jpg",
+            "images/homeItems/curtains3.jpg",
+            "images/homeItems/curtains4.jpeg"
+          ]
+        },
+        {
+          "name": "Cushion",
+          "images": [
+            "images/homeItems/cushion1.jpg",
+            "images/homeItems/cushion2.jpg",
+            "images/homeItems/cushion3.jpg",
+            "images/homeItems/cushion4.jpg"
+          ]
+        },
+        {
+          "name": "Doorbell",
+          "images": [
+            "images/homeItems/doorbell1.jpg",
+            "images/homeItems/doorbell2.jpg",
+            "images/homeItems/doorbell3.jpg",
+            "images/homeItems/doorbell4.jpg"
+          ]
+        },
+        {
+          "name": "Lamp",
+          "images": [
+            "images/homeItems/lamp1.jpg",
+            "images/homeItems/lamp2.jpg",
+            "images/homeItems/lamp3.jpeg",
+            "images/homeItems/lamp4.jpg"
+          ]
+        },
+        {
+          "name": "Letterbox",
+          "images": [
+            "images/homeItems/letterbox1.jpg",
+            "images/homeItems/letterbox2.jpg",
+            "images/homeItems/letterbox3.jpg",
+            "images/homeItems/letterbox4.jpg"
+          ]
+        },
+        {
+          "name": "Shower",
+          "images": [
+            "images/homeItems/shower1.jpg",
+            "images/homeItems/shower2.jpg",
+            "images/homeItems/shower3.jpg",
+            "images/homeItems/shower4.jpg"
+          ]
+        },
+        {
+          "name": "Sink",
+          "images": [
+            "images/homeItems/sink1.jpeg",
+            "images/homeItems/sink2.jpg",
+            "images/homeItems/sink3.jpeg",
+            "images/homeItems/sink4.jpg"
+          ]
+        },
+        {
+          "name": "Sofa",
+          "images": [
+            "images/homeItems/sofa1.jpg",
+            "images/homeItems/sofa2.jpg",
+            "images/homeItems/sofa3.jpg",
+            "images/homeItems/sofa4.jpg"
+          ]
+        },
+        {
+          "name": "Stairs",
+          "images": [
+            "images/homeItems/stairs1.jpg",
+            "images/homeItems/stairs2.jpg",
+            "images/homeItems/stairs3.jpg",
+            "images/homeItems/stairs4.jpg"
+          ]
+        },
+        {
+          "name": "Table",
+          "images": [
+            "images/homeItems/table1.jpeg",
+            "images/homeItems/table2.jpg",
+            "images/homeItems/table3.jpg",
+            "images/homeItems/table4.jpg"
+          ]
+        },
+        {
+          "name": "Toilet",
+          "images": [
+            "images/homeItems/toilet1.jpg",
+            "images/homeItems/toilet2.jpg",
+            "images/homeItems/toilet3.jpg",
+            "images/homeItems/toilet4.jpg"
+          ]
+        },
+        {
+          "name": "Wardrobe",
+          "images": [
+            "images/homeItems/wardrobe1.jpg",
+            "images/homeItems/wardrobe2.jpg",
+            "images/homeItems/wardrobe3.jpg",
+            "images/homeItems/wardrobe4.jpeg"
+          ]
+        }
+      ],
+      "builtin": true,
+      "editable": true,
+      "showQuestionText": true
+    },
+    {
+      "name": "Jungle Animals",
+      "stimulus": [
+        {
+          "name": "Bat",
+          "images": [
+            "images/jungleAnimals/bat1.jpg",
+            "images/jungleAnimals/bat2.jpg",
+            "images/jungleAnimals/bat3.jpg",
+            "images/jungleAnimals/bat4.jpg"
+          ]
+        },
+        {
+          "name": "Crocodile",
+          "images": [
+            "images/jungleAnimals/crocodile1.jpeg",
+            "images/jungleAnimals/crocodile2.jpg",
+            "images/jungleAnimals/crocodile3.jpg",
+            "images/jungleAnimals/crocodile4.jpg"
+          ]
+        },
+        {
+          "name": "Frog",
+          "images": [
+            "images/jungleAnimals/frog1.jpg",
+            "images/jungleAnimals/frog2.jpg",
+            "images/jungleAnimals/frog3.jpg",
+            "images/jungleAnimals/frog4.jpg"
+          ]
+        },
+        {
+          "name": "Monkey",
+          "images": [
+            "images/jungleAnimals/monkey1.jpg",
+            "images/jungleAnimals/monkey2.jpg",
+            "images/jungleAnimals/monkey3.jpg",
+            "images/jungleAnimals/monkey4.jpeg"
+          ]
+        },
+        {
+          "name": "Parrot",
+          "images": [
+            "images/jungleAnimals/parrot1.jpg",
+            "images/jungleAnimals/parrot2.jpg",
+            "images/jungleAnimals/parrot3.jpg",
+            "images/jungleAnimals/parrot4.jpg"
+          ]
+        },
+        {
+          "name": "Snake",
+          "images": [
+            "images/jungleAnimals/snake1.jpg",
+            "images/jungleAnimals/snake2.jpg",
+            "images/jungleAnimals/snake3.jpg",
+            "images/jungleAnimals/snake4.jpeg"
+          ]
+        },
+        {
+          "name": "Tapir",
+          "images": [
+            "images/jungleAnimals/tapir1.jpg",
+            "images/jungleAnimals/tapir2.jpg",
+            "images/jungleAnimals/tapir3.png",
+            "images/jungleAnimals/tapir4.jpeg"
+          ]
+        },
+        {
+          "name": "Toucan",
+          "images": [
+            "images/jungleAnimals/toucan1.jpg",
+            "images/jungleAnimals/toucan2.jpg",
+            "images/jungleAnimals/toucan3.jpg",
+            "images/jungleAnimals/toucan4.jpg"
+          ]
+        }
+      ],
+      "builtin": true,
+      "editable": true,
+      "showQuestionText": true
+    },
+    {
+      "name": "Vegetables",
+      "stimulus": [
+        {
+          "name": "Brussel Sprouts",
+          "images": [
+            "images/veg/bsprouts1.jpg",
+            "images/veg/bsprouts2.jpg",
+            "images/veg/bsprouts3.jpg",
+            "images/veg/bsprouts4.jpg"
+          ]
+        },
+        {
+          "name": "Carrots",
+          "images": [
+            "images/veg/carrots1.jpg",
+            "images/veg/carrots2.jpg",
+            "images/veg/carrots3.jpg",
+            "images/veg/carrots4.jpg"
+          ]
+        },
+        {
+          "name": "Cauliflower",
+          "images": [
+            "images/veg/cauli1.jpg",
+            "images/veg/cauli2.jpg",
+            "images/veg/cauli3.jpg",
+            "images/veg/cauli4.jpg"
+          ]
+        },
+        {
+          "name": "Celery",
+          "images": [
+            "images/veg/celery1.jpg",
+            "images/veg/celery2.jpg",
+            "images/veg/celery3.jpg",
+            "images/veg/celery4.jpg"
+          ]
+        },
+        {
+          "name": "Cucumber",
+          "images": [
+            "images/veg/cucumber1.jpg",
+            "images/veg/cucumber2.jpg",
+            "images/veg/cucumber3.jpg",
+            "images/veg/cucumber4.jpg"
+          ]
+        },
+        {
+          "name": "Peppers",
+          "images": [
+            "images/veg/peppers1.jpg",
+            "images/veg/peppers2.jpg",
+            "images/veg/peppers3.jpg",
+            "images/veg/peppers4.jpg"
+          ]
+        },
+        {
+          "name": "Potato",
+          "images": [
+            "images/veg/potato1.jpg",
+            "images/veg/potato2.jpg",
+            "images/veg/potato3.jpg",
+            "images/veg/potato4.jpeg"
+          ]
+        },
+        {
+          "name": "Runner Beans",
+          "images": [
+            "images/veg/rbeans1.jpg",
+            "images/veg/rbeans2.jpg",
+            "images/veg/rbeans3.jpg",
+            "images/veg/rbeans4.jpg"
+          ]
+        }
+      ],
+      "builtin": true,
+      "editable": true,
+      "showQuestionText": true
+    },
+    {
+      "name": "Vehicles",
+      "stimulus": [
+        {
+          "name": "Boat",
+          "images": [
+            "images/vehicles/boat1.jpeg",
+            "images/vehicles/boat2.jpg",
+            "images/vehicles/boat3.jpg"
+          ]
+        },
+        {
+          "name": "Bus",
+          "images": [
+            "images/vehicles/bus1.jpg",
+            "images/vehicles/bus2.jpg",
+            "images/vehicles/bus3.jpg"
+          ]
+        },
+        {
+          "name": "Car",
+          "images": [
+            "images/vehicles/car1.jpg",
+            "images/vehicles/car2.jpg",
+            "images/vehicles/car3.jpeg"
+          ]
+        },
+        {
+          "name": "Plane",
+          "images": [
+            "images/vehicles/plane1.jpeg",
+            "images/vehicles/plane2.jpeg",
+            "images/vehicles/plane3.jpg"
+          ]
+        },
+        {
+          "name": "Tractor",
+          "images": [
+            "images/vehicles/trac1.png",
+            "images/vehicles/trac2.jpg",
+            "images/vehicles/trac3.jpg"
+          ]
+        },
+        {
+          "name": "Train",
+          "images": [
+            "images/vehicles/train1.jpg",
+            "images/vehicles/train2.jpg",
+            "images/vehicles/train3.jpg"
+          ]
+        }
+      ],
+      "custom": true,
+      "editable": true,
+      "builtin": true,
+      "showQuestionText": true
+    },
+    {
+      "name": "Days of the Week",
+      "stimulus": [
+        {
+          "name": "Monday",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "Tuesday",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "Wednesday",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "Thursday",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "Friday",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "Saturday",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "Sunday",
+          "images": [],
+          "textOnly": true
+        }
+      ],
+      "custom": true,
+      "editable": true,
+      "showQuestionText": false,
+      "textOnly": true,
+      "builtin": true
+    },
+    {
+      "name": "Letter Names",
+      "stimulus": [
+        {
+          "name": "a",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "b",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "c",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "d",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "e",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "f",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "g",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "h",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "i",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "j",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "k",
+          "images": [],
+          "textOnly": true
+        },
+        {
+          "name": "l",
+          "images": [],
+          "textOnly": true
+        }
+      ],
+      "custom": true,
+      "editable": true,
+      "showQuestionText": true,
+      "textOnly": true
+    }
+  ]
+};
+
+// Make available for use
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = quizData;
+}
